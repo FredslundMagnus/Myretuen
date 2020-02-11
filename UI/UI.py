646d91209b9d691b9ab565e385e37e559e63cfe1
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-02-11 18:42:31 +0100

    Start UI

--- UI/UI.py
+++ UI/UI.py
@@ -1,1 +1,21 @@
 import pygame
+import UI.field
+
+pygame.init()
+size = (480, 600)
+win = pygame.display.set_mode(size)
+pygame.display.set_caption("Myretuen")
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    win.fill((0, 0, 0))
+    # Do soething
+    pygame.display.update()
+    pygame.time.delay(round(1000/60))
+
+
+pygame.quit()
