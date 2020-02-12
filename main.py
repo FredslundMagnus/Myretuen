3d3e3cabfa999ad4c2739cc3ebaaf4103002d4c2
s183914 <s183914@student.dtu.dk>
2020-02-12 15:57:18 +0100

    Nye_felter

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from UI.UI import draw
 from ant import generateAnts
 from lines import generateLines
 
-fields = {'K1': Field(80, 80, color='blueFade'),
-          'K2': Field(75, 100, color='redFade'),
-          'K3': Field(95, 110, color='yellowFade'),
+fields = {'base1': Field(20,310, color='yellowFade', special='base'),
           'D1': Field(60, 320, color='yellowFade'),
           'D2': Field(80, 320, color='yellowFade'),
           'D3': Field(100, 310, color='yellowFade'),
+          'D4': Field(120, 320, color='yellowFade'),
+          'D5': Field(140, 320, color='yellowFade'),
+          'D6': Field(160, 310, color='yellowFade'),
+          'D7': Field(180, 300, color='yellowFade'),
+          'D8': Field(190, 280, color='yellowFade'),
 
           'A1': Field(30, 290, color='yellowFade'),
           'A2': Field(30, 270, color='yellowFade'),
           'A3': Field(40, 250, color='yellowFade'),
+          'A4': Field(40, 250, color='yellowFade'),
+          'A5': Field(40, 250, color='yellowFade')
           }
 
 giveFieldsID(fields)
