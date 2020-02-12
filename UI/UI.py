eba44839501434ad021fea9e5877fe6a6d5e9cb8
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-02-12 15:53:09 +0100

    Clean UI

--- UI/UI.py
+++ UI/UI.py
@@ -10,27 +10,29 @@ colors = {'blueFade': (179, 180, 208), 'redFade': (
 
 
 class Field():
-    def __init__(self, x: int, y: int, scale: int, win, color=(255, 255, 255), special=None, rotation=0):
-        if special == 'base':
+    def __init__(self, field, scale: int, win):
+        if field.special == 'base':
             scale *= 2
-        self.rect = pygame.Rect(x*scale, y*scale, 19*scale, 19*scale)
-        self.border = pygame.Rect((x-1)*scale, (y-1)*scale, 21*scale, 21*scale)
+        self.rect = pygame.Rect(
+            field.x*scale, field.y*scale, 19*scale, 19*scale)
+        self.border = pygame.Rect(
+            (field.x-1)*scale, (field.y-1)*scale, 21*scale, 21*scale)
         self.shadow1 = pygame.Rect(
-            (x-10)*scale, (y-10)*scale, 39*scale, 39*scale)
+            (field.x-10)*scale, (field.y-10)*scale, 39*scale, 39*scale)
         self.shadow2 = pygame.Rect(
-            (x-3)*scale, (y-3)*scale, 25*scale, 25*scale)
+            (field.x-3)*scale, (field.y-3)*scale, 25*scale, 25*scale)
 
         self.win = win
-        self.color = color
-        self.special = special
-        self.rotation = rotation
-        self.x = x
-        self.y = y
+        self.color = field.color
+        self.special = field.special
+        self.rotation = field.rotation
+        self.x = field.x
+        self.y = field.y
         self.scale = scale
 
     def draw(self):
         pygame.draw.rect(self.win, (0, 0, 0), self.border)
-        pygame.draw.rect(self.win, self.color, self.rect)
+        pygame.draw.rect(self.win, colors[self.color], self.rect)
         if self.special != None:
             img = pygame.image.load('UI/flag.png').convert_alpha()
             self.win.blit(pygame.transform.rotate(pygame.transform.scale(
@@ -43,49 +45,6 @@ class Field():
         pygame.draw.rect(self.win, (43, 22, 17), self.shadow2)
 
 
-# fields = [
-#     Field(80, 120, scale, win, color=colors['blueFade']),
-#     Field(60, 120, scale, win, color=colors['redFade']),
-#     Field(50, 140, scale, win, color=colors['greenFade']),
-#     Field(40, 160, scale, win, color=colors['blueFade']),
-#     Field(50, 180, scale, win, color=colors['yellowFade']),
-#     Field(60, 200, scale, win, color=colors['greenFade']),
-#     Field(70, 220, scale, win, color=colors['blueFade']),
-#     Field(55, 240, scale, win, color=colors['redFade']),
-#     Field(75, 250, scale, win, color=colors['greenFade']),
-#     Field(80, 270, scale, win, color=colors['yellowFade']),
-
-#     Field(85, 290, scale, win,
-#           color=colors['blue'], special='Flag', rotation=180),
-#     Field(92, 310, scale, win, color=colors['blue']),
-#     Field(72, 330, scale, win, color=colors['blue']),
-#     Field(92, 330, scale, win, color=colors['blue']),
-#     Field(112, 330, scale, win, color=colors['blue']),
-#     Field(132, 330, scale, win,
-#           color=colors['blue'], special='Flag', rotation=90),
-#     Field(72, 350, scale, win, color=colors['blue']),
-#     Field(92, 350, scale, win, color=colors['blue']),
-#     Field(112, 350, scale, win, color=colors['blue']),
-
-#     Field(100, 265, scale, win, color=colors['redFade']),
-#     Field(120, 262, scale, win, color=colors['blueFade']),
-#     Field(113, 242, scale, win, color=colors['greenFade']),
-#     Field(120, 222, scale, win, color=colors['redFade']),
-#     Field(140, 235, scale, win, color=colors['yellowFade']),
-#     Field(145, 215, scale, win, color=colors['greenFade']),
-#     Field(145, 195, scale, win, color=colors['redFade']),
-#     Field(140, 175, scale, win, color=colors['blueFade']),
-#     Field(135, 155, scale, win, color=colors['yellowFade']),
-#     Field(115, 147, scale, win, color=colors['redFade']),
-#     Field(95, 160, scale, win, color=colors['greenFade']),
-#     Field(85, 140, scale, win, color=colors['yellowFade']),
-
-#     Field(150, 255, scale, win, color=colors['blueFade']),
-
-#     Field(152, 340, scale, win, color=colors['blueFade']),
-# ]
-
-
 def draw(fields):
 
     pygame.init()
@@ -93,8 +52,7 @@ def draw(fields):
     win = pygame.display.set_mode(size)
     pygame.display.set_caption("Myretuen")
     run = True
-    fields = [Field(fields[field].x, fields[field].y, scale, win, color=colors[fields[field].color])
-              for field in fields]
+    fields = [Field(field, scale, win) for key, field in fields.items()]
 
     win.fill((190, 161, 43))
     for field in fields:
