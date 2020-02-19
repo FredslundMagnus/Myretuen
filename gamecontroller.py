95fb1143dd14eec0f25eb5bbb6b0951e59530025
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-02-19 20:29:47 +0100

    CleanAgents

--- gamecontroller.py
+++ gamecontroller.py
@@ -7,22 +7,21 @@ class Gamecontroller():
     def __init__(self, game=None, agent1=None, agent2=None, timeDelay=0):
         self.timeDelay = timeDelay
         self.game = game
-        self.agent1 = agent1
-        self.agent2 = agent2
+        self.agents = [agent1, agent2]
 
     def run(self):
         thread = threading.currentThread()
-        currentAgent = self.agent1
+        currentAgent = 0
         while getattr(thread, "do_run", True):
             time.sleep(self.timeDelay)
             self.game.roll()
             changeThisPlayer = len(set(self.game.rolled)) == 2  # and False
             time.sleep(self.timeDelay)
             actions = list(self.game.actions())
-            currentAgent.choose(actions)
+            self.agents[currentAgent].choose(actions)
             time.sleep(self.timeDelay)
             actions = list(self.game.actions())
-            currentAgent.choose(actions)
+            self.agents[currentAgent].choose(actions)
             if changeThisPlayer:
                 self.game.currentPlayer = self.game.player2 if self.game.currentPlayer == self.game.player1 else self.game.player1
-                currentAgent = self.agent2 if currentAgent == self.agent1 else self.agent1
+                currentAgent = 1 if currentAgent == 0 else 0
