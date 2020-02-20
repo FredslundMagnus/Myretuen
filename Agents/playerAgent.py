520a72669bf3b8710a9cd1fbc29a4ba6dc3a4d83
FredslundMagnus <s183905@student.dtu.dk>
2020-02-20 13:19:30 +0100

    fix

--- Agents/playerAgent.py
+++ Agents/playerAgent.py
@@ -6,7 +6,7 @@ class KeyboardAgent():
             actionsEnd = [action for action in actions if 'Base' in str(action)]
         else:
             actionsEnd = [action for action in actions if f'Field({start}) to' in str(action)]
-        [print(action) for action in actionsEnd]
+        print([str(action).split(' ')[-1][6:-2] for action in actionsEnd])
         end = input('Choose your landing position: ').capitalize()
         action = [action for action in actionsEnd if f'to Field({end})' in str(action)]
         action[0].execute()
