cfd04c346c70a6b0f1f7f89fd979a3bc1b04eb23
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-02-29 18:06:08 +0100

    simple

--- Agents/simpleLinear.py
+++ Agents/simpleLinear.py
@@ -12,16 +12,19 @@ class SimpleLinear(Agent):
         Nfeature = np.array(state).shape[-1]
         if len(self.phi) == 0:
             self.phi = np.random.rand(Nfeature)
-        x = np.array(state).reshape(-1, Nfeature) @ self.phi
-        factor = np.concatenate((np.ones(len(x)//2), -np.ones(len(x)//2)), axis=0)
-        return np.array(state).reshape(-1, Nfeature) @ self.phi @ factor
+        x = np.array(state).reshape(-1, Nfeature)
+        factor = np.concatenate((np.ones(x.shape[0]//2), -np.ones(x.shape[0]//2)), axis=0)
+        return x @ self.phi @ factor
 
     def train(self, reward, action, observation, alpha=0.000001, discount=0.8):
         if len(self.previousState) == 0 or action == None:
             return
         Vst = self.value(self.previousState)
-        Vstnext = self.value(self.state(observation))
-        x = np.array(self.state(observation)).reshape(-1)
-        self.phi += alpha * (reward + discount * Vstnext - Vst) * x
+        state = self.state(observation)
+        Vstnext = self.value(state)
+        Nfeature = np.array(state).shape[-1]
+        x = np.array(state).reshape(-1, Nfeature)
+        factor = np.concatenate((np.ones(x.shape[0]//2), -np.ones(x.shape[0]//2)), axis=0)
+        self.phi += alpha * (reward + discount * Vstnext - Vst) * (factor @ x)
         self.previousState = []
-        #print(self.phi @ np.array(self.actionState).reshape(-1))
+        # print(self.phi @ np.array(self.actionState).reshape(-1))
