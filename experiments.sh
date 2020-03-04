89ad1375c98eb9d1b4e86ab48f8f6f9367fb5e28
FredslundMagnus <s183905@student.dtu.dk>
2020-03-04 16:37:12 +0100

    Klar

--- experiments.sh
+++ experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-bsub -P "test1 5" < submit.sh
-bsub -P "test2 5" < submit.sh
-bsub -P "test3 5" < submit.sh
-bsub -P "test4 5" < submit.sh
-bsub -P "test5 5" < submit.sh
-bsub -P "test6 5" < submit.sh
+bsub -P "test1NY 10" < submit.sh
+bsub -P "test2NY 10" < submit.sh
+bsub -P "test3NY 10" < submit.sh
+bsub -P "test4NY 10" < submit.sh
+bsub -P "test5NY 10" < submit.sh
+bsub -P "test6NY 10" < submit.sh
