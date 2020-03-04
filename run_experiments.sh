e7cd75fe28104f7787deb86a4e9a0f01b29c794c
FredslundMagnus <s183905@student.dtu.dk>
2020-03-04 16:19:49 +0100

    clean

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-bsub < submit.sh
-bsub < submit.sh
-bsub < submit.sh
-bsub < submit.sh
-bsub < submit.sh
+bsub -P "test1 5" < submit.sh
+bsub -P "test2 5" < submit.sh
+bsub -P "test3 5" < submit.sh
+bsub -P "test4 5" < submit.sh
+bsub -P "test5 5" < submit.sh
+bsub -P "test6 5" < submit.sh
