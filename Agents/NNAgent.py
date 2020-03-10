982692665ce3548fc4db3043e415fdf5a68ea43c
s183914 <s183914@student.dtu.dk>
2020-03-10 16:23:27 +0100

    Hello

--- Agents/NNAgent.py
+++ Agents/NNAgent.py
@@ -42,7 +42,7 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(46, 20)
+        self.fc1 = nn.Linear(45, 20)
         self.fc2 = nn.Linear(20, 10)
         self.fc3 = nn.Linear(10, 10)
         self.fc4 = nn.Linear(10, 1)
