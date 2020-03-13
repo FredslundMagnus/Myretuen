7491696ebcadf377f4615a06710cef1190e4773e
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-13 14:19:04 +0100

    test4

--- generateExperiments.py
+++ generateExperiments.py
@@ -1,9 +1,11 @@
+
 file = open('experiments.sh', 'w')
 file.write('#!/bin/sh\n')
 agent = 'NNAgent'
-name = 'Test3'
+name = 'Test4'
 gameLength = 50
 explore, doTrain, impala, calcprobs = True, True, True, True
 
+file.write(f"mkdir outputs/{name}\n")
 for i in range(10):
-    file.write(f'bsub -o "outputs/{agent}{str(i)}{name}.out" -J "{agent}{str(i)}{name}" -P "{agent}{str(i)}{name} {gameLength} 10 {agent} {int(explore)} {int(doTrain)} {int(impala)} {int(calcprobs)}" < submit.sh\n')
+    file.write(f'bsub -o "outputs/{name}/{agent}{str(i)}{name}.out" -J "{agent}{str(i)}{name}" -P "{agent}{str(i)}{name} {gameLength} 10 {agent} {int(explore)} {int(doTrain)} {int(impala)} {int(calcprobs)}" < submit.sh\n')
