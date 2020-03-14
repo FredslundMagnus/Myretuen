17d8f5aadfb812500305d4d3a44d4e84fe0847c3
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-14 19:03:19 +0100

    Elo random

--- Agents/randomAgent.py
+++ Agents/randomAgent.py
@@ -5,9 +5,13 @@ from Agents.agent import Agent
 class RandomAgent(Agent):
     def __init__(self, explore=False, doTrain=False, impala=False, calcprobs=True):
         self.setup(explore, doTrain, impala, calcprobs, name='RandomAgent')
+        self.rating = 1000
 
     def choose(self, actions):
         if len(actions) != 0:
             action = random.choice(actions)
             return action
         return None
+
+    def resetGame(self):
+        self.rating = 1000
