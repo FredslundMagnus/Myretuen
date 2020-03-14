47d3c2c76d035db5a437ffe3fe8405bde3c06612
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-14 20:06:38 +0100

    test

--- experiments.sh
+++ experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-mkdir outputs/NyExploration100
-bsub -o "outputs/NyExploration100/NNAgent0NyExploration100.md" -J "NNAgent0NyExploration100" -P "NNAgent0NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent1NyExploration100.md" -J "NNAgent1NyExploration100" -P "NNAgent1NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent2NyExploration100.md" -J "NNAgent2NyExploration100" -P "NNAgent2NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent3NyExploration100.md" -J "NNAgent3NyExploration100" -P "NNAgent3NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent4NyExploration100.md" -J "NNAgent4NyExploration100" -P "NNAgent4NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent5NyExploration100.md" -J "NNAgent5NyExploration100" -P "NNAgent5NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent6NyExploration100.md" -J "NNAgent6NyExploration100" -P "NNAgent6NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent7NyExploration100.md" -J "NNAgent7NyExploration100" -P "NNAgent7NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent8NyExploration100.md" -J "NNAgent8NyExploration100" -P "NNAgent8NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
-bsub -o "outputs/NyExploration100/NNAgent9NyExploration100.md" -J "NNAgent9NyExploration100" -P "NNAgent9NyExploration100 1000 10 NNAgent 1 1 1 1 NyExploration100" < submit.sh
+mkdir outputs/NyExploration50
+bsub -o "outputs/NyExploration50/NNAgent0NyExploration50.md" -J "NNAgent0NyExploration50" -P "NNAgent0NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent1NyExploration50.md" -J "NNAgent1NyExploration50" -P "NNAgent1NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent2NyExploration50.md" -J "NNAgent2NyExploration50" -P "NNAgent2NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent3NyExploration50.md" -J "NNAgent3NyExploration50" -P "NNAgent3NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent4NyExploration50.md" -J "NNAgent4NyExploration50" -P "NNAgent4NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent5NyExploration50.md" -J "NNAgent5NyExploration50" -P "NNAgent5NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent6NyExploration50.md" -J "NNAgent6NyExploration50" -P "NNAgent6NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent7NyExploration50.md" -J "NNAgent7NyExploration50" -P "NNAgent7NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent8NyExploration50.md" -J "NNAgent8NyExploration50" -P "NNAgent8NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
+bsub -o "outputs/NyExploration50/NNAgent9NyExploration50.md" -J "NNAgent9NyExploration50" -P "NNAgent9NyExploration50 1000 10 NNAgent 1 1 1 1 NyExploration50" < submit.sh
