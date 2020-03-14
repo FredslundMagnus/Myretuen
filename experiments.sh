df66961ecc1b0353697707796299d454c9290ff5
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-14 14:35:46 +0100

    test6

--- experiments.sh
+++ experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-mkdir outputs/Test5
-bsub -o "outputs/Test5/NNAgent0Test5.md" -J "NNAgent0Test5" -P "NNAgent0Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent1Test5.md" -J "NNAgent1Test5" -P "NNAgent1Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent2Test5.md" -J "NNAgent2Test5" -P "NNAgent2Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent3Test5.md" -J "NNAgent3Test5" -P "NNAgent3Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent4Test5.md" -J "NNAgent4Test5" -P "NNAgent4Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent5Test5.md" -J "NNAgent5Test5" -P "NNAgent5Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent6Test5.md" -J "NNAgent6Test5" -P "NNAgent6Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent7Test5.md" -J "NNAgent7Test5" -P "NNAgent7Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent8Test5.md" -J "NNAgent8Test5" -P "NNAgent8Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
-bsub -o "outputs/Test5/NNAgent9Test5.md" -J "NNAgent9Test5" -P "NNAgent9Test5 20 10 NNAgent 1 1 1 1 Test5" < submit.sh
+mkdir outputs/Test6
+bsub -o "outputs/Test6/NNAgent0Test6.md" -J "NNAgent0Test6" -P "NNAgent0Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent1Test6.md" -J "NNAgent1Test6" -P "NNAgent1Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent2Test6.md" -J "NNAgent2Test6" -P "NNAgent2Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent3Test6.md" -J "NNAgent3Test6" -P "NNAgent3Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent4Test6.md" -J "NNAgent4Test6" -P "NNAgent4Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent5Test6.md" -J "NNAgent5Test6" -P "NNAgent5Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent6Test6.md" -J "NNAgent6Test6" -P "NNAgent6Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent7Test6.md" -J "NNAgent7Test6" -P "NNAgent7Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent8Test6.md" -J "NNAgent8Test6" -P "NNAgent8Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
+bsub -o "outputs/Test6/NNAgent9Test6.md" -J "NNAgent9Test6" -P "NNAgent9Test6 20 10 NNAgent 1 1 1 1 Test6" < submit.sh
--- main.py
+++ main.py
@@ -1,62 +1,10 @@
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+from debug import debugger
 from matplotlib import pyplot as plt
 import numpy as np
 from agents import *
 from game import Myretuen, Controller
-import cProfile
-import pstats
-from pstats import func_get_function_name, func_std_string, f8
 import sys
-import time
-
-
-def print_title(self):
-    print('           ncalls  tottime  percall  cumtime  percall', end=' ', file=self.stream)
-    print('filename:lineno(function)', file=self.stream)
-
-
-def print_line(self, func):  # hack: should print percentages
-    cc, nc, tt, ct, callers = self.stats[func]
-    c = str(nc)
-    if nc != cc:
-        c = c + '/' + str(cc)
-    print('       ', c.rjust(9), end=' ', file=self.stream)
-    print(f8(tt), end=' ', file=self.stream)
-    if nc == 0:
-        print(' ' * 8, end=' ', file=self.stream)
-    else:
-        print(f8(tt / nc), end=' ', file=self.stream)
-    print(f8(ct), end=' ', file=self.stream)
-    if cc == 0:
-        print(' ' * 8, end=' ', file=self.stream)
-    else:
-        print(f8(ct / cc), end=' ', file=self.stream)
-    print(func_std_string(func), file=self.stream)
-
-
-def print_stats(self, *amount):
-    # for filename in self.files:
-    #     print(filename, file=self.stream)
-    if self.files:
-        print(file=self.stream)
-    indent = ' ' * 5
-    for func in self.top_level:
-        print(indent, func_get_function_name(func), file=self.stream)
-
-    print(indent, self.total_calls, "function calls", end=' ', file=self.stream)
-    if self.total_calls != self.prim_calls:
-        print("(%d primitive calls)" % self.prim_calls, end=' ', file=self.stream)
-    print("in %.3f seconds" % self.total_tt, file=self.stream)
-    print(file=self.stream)
-    width, list = self.get_print_list(amount)
-    if list:
-        print_title(self)
-        for func in list:
-            print_line(self, func)
-        print(file=self.stream)
-        print(file=self.stream)
-    return self
+import os
 
 
 debuggerMode = len(sys.argv) != 1
@@ -72,25 +20,7 @@ if debuggerMode:
     sys.stdout = open(os.devnull, 'w')
     env = Myretuen()
     controller = Controller(env=env, agent1=Opponent(RandomAgent()), agent2=ourAgent(explore=explore, doTrain=doTrain, impala=impala, calcprobs=calcprobs))
-    start = time.time()
-    cProfile.run(f'controller.run(NGames={nGames}, AddAgent={addAgent}, UI=False)', 'stats')
-    end = time.time()
-    sys.stdout = sys.__stdout__
-    print(f"# Parameters for {Thename}\n")
-    print(f"    Use the agent :             {sys.argv[4]}.")
-    print(f"    Play for :                  {nGames} games.")
-    print(f"    Add Agent every :           {addAgent} game.")
-    print(f'    Explore enabled :           {str(explore)}.')
-    print(f'    DoTrain enabled :           {str(doTrain)}.')
-    print(f'    Impala enabled :            {str(impala)}.')
-    print(f'    Calcprobs enabled :         {str(calcprobs)}.')
-    print(f'    Time used :                 {(end-start)//60} minutes.\n')
-    print(f"# Profiling\n")
-    p = pstats.Stats('stats')
-    p.print_stats = print_stats
-    p.strip_dirs().sort_stats('cumulative').print_stats(p)
-    os.remove('stats')
-    print(f"# Other prints\n")
+    debugger(nGames, addAgent, Thename, explore, doTrain, impala, calcprobs)
 else:
     env = Myretuen()
     controller = Controller(env=env, agent1=Opponent(RandomAgent()), agent2=NNAgent())
