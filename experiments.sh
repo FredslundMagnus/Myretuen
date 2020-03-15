da913ef12d4d7f478b2a2d780e2a58ba3ddbab8e
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-15 22:12:53 +0100

    test

--- experiments.sh
+++ experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-mkdir outputs/MinMax-5-1
-bsub -o "outputs/MinMax-5-1/NNAgent0MinMax-5-1.md" -J "NNAgent0MinMax-5-1" -P "NNAgent0MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent1MinMax-5-1.md" -J "NNAgent1MinMax-5-1" -P "NNAgent1MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent2MinMax-5-1.md" -J "NNAgent2MinMax-5-1" -P "NNAgent2MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent3MinMax-5-1.md" -J "NNAgent3MinMax-5-1" -P "NNAgent3MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent4MinMax-5-1.md" -J "NNAgent4MinMax-5-1" -P "NNAgent4MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent5MinMax-5-1.md" -J "NNAgent5MinMax-5-1" -P "NNAgent5MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent6MinMax-5-1.md" -J "NNAgent6MinMax-5-1" -P "NNAgent6MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent7MinMax-5-1.md" -J "NNAgent7MinMax-5-1" -P "NNAgent7MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent8MinMax-5-1.md" -J "NNAgent8MinMax-5-1" -P "NNAgent8MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
-bsub -o "outputs/MinMax-5-1/NNAgent9MinMax-5-1.md" -J "NNAgent9MinMax-5-1" -P "NNAgent9MinMax-5-1 500 10 NNAgent 0 1 1 1 1 MinMax-5-1" < submit.sh
+mkdir outputs/MinMax-off
+bsub -o "outputs/MinMax-off/NNAgent0MinMax-off.md" -J "NNAgent0MinMax-off" -P "NNAgent0MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent1MinMax-off.md" -J "NNAgent1MinMax-off" -P "NNAgent1MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent2MinMax-off.md" -J "NNAgent2MinMax-off" -P "NNAgent2MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent3MinMax-off.md" -J "NNAgent3MinMax-off" -P "NNAgent3MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent4MinMax-off.md" -J "NNAgent4MinMax-off" -P "NNAgent4MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent5MinMax-off.md" -J "NNAgent5MinMax-off" -P "NNAgent5MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent6MinMax-off.md" -J "NNAgent6MinMax-off" -P "NNAgent6MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent7MinMax-off.md" -J "NNAgent7MinMax-off" -P "NNAgent7MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent8MinMax-off.md" -J "NNAgent8MinMax-off" -P "NNAgent8MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
+bsub -o "outputs/MinMax-off/NNAgent9MinMax-off.md" -J "NNAgent9MinMax-off" -P "NNAgent9MinMax-off 500 10 NNAgent 0 1 1 1 0 MinMax-off" < submit.sh
