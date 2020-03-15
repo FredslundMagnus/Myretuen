bf588e6958f2d61eff46c849dfe80dac19d82c50
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-15 22:17:32 +0100

    test

--- experiments.sh
+++ experiments.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
-mkdir outputs/Explorer-off
-bsub -o "outputs/Explorer-off/NNAgent0Explorer-off.md" -J "NNAgent0Explorer-off" -P "NNAgent0Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent1Explorer-off.md" -J "NNAgent1Explorer-off" -P "NNAgent1Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent2Explorer-off.md" -J "NNAgent2Explorer-off" -P "NNAgent2Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent3Explorer-off.md" -J "NNAgent3Explorer-off" -P "NNAgent3Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent4Explorer-off.md" -J "NNAgent4Explorer-off" -P "NNAgent4Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent5Explorer-off.md" -J "NNAgent5Explorer-off" -P "NNAgent5Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent6Explorer-off.md" -J "NNAgent6Explorer-off" -P "NNAgent6Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent7Explorer-off.md" -J "NNAgent7Explorer-off" -P "NNAgent7Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent8Explorer-off.md" -J "NNAgent8Explorer-off" -P "NNAgent8Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-bsub -o "outputs/Explorer-off/NNAgent9Explorer-off.md" -J "NNAgent9Explorer-off" -P "NNAgent9Explorer-off 2000 10 NNAgent 0 1 1 1 0 Explorer-off" < submit.sh
-mkdir outputs/Explorer-on-no-temp
-bsub -o "outputs/Explorer-on-no-temp/NNAgent0Explorer-on-no-temp.md" -J "NNAgent0Explorer-on-no-temp" -P "NNAgent0Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent1Explorer-on-no-temp.md" -J "NNAgent1Explorer-on-no-temp" -P "NNAgent1Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent2Explorer-on-no-temp.md" -J "NNAgent2Explorer-on-no-temp" -P "NNAgent2Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent3Explorer-on-no-temp.md" -J "NNAgent3Explorer-on-no-temp" -P "NNAgent3Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent4Explorer-on-no-temp.md" -J "NNAgent4Explorer-on-no-temp" -P "NNAgent4Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent5Explorer-on-no-temp.md" -J "NNAgent5Explorer-on-no-temp" -P "NNAgent5Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent6Explorer-on-no-temp.md" -J "NNAgent6Explorer-on-no-temp" -P "NNAgent6Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent7Explorer-on-no-temp.md" -J "NNAgent7Explorer-on-no-temp" -P "NNAgent7Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent8Explorer-on-no-temp.md" -J "NNAgent8Explorer-on-no-temp" -P "NNAgent8Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
-bsub -o "outputs/Explorer-on-no-temp/NNAgent9Explorer-on-no-temp.md" -J "NNAgent9Explorer-on-no-temp" -P "NNAgent9Explorer-on-no-temp 2000 10 NNAgent 1 1 1 1 0 Explorer-on-no-temp" < submit.sh
+mkdir outputs/Explorer-K-10
+bsub -o "outputs/Explorer-K-10/NNAgent0Explorer-K-10.md" -J "NNAgent0Explorer-K-10" -P "NNAgent0Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent1Explorer-K-10.md" -J "NNAgent1Explorer-K-10" -P "NNAgent1Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent2Explorer-K-10.md" -J "NNAgent2Explorer-K-10" -P "NNAgent2Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent3Explorer-K-10.md" -J "NNAgent3Explorer-K-10" -P "NNAgent3Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent4Explorer-K-10.md" -J "NNAgent4Explorer-K-10" -P "NNAgent4Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent5Explorer-K-10.md" -J "NNAgent5Explorer-K-10" -P "NNAgent5Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent6Explorer-K-10.md" -J "NNAgent6Explorer-K-10" -P "NNAgent6Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent7Explorer-K-10.md" -J "NNAgent7Explorer-K-10" -P "NNAgent7Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent8Explorer-K-10.md" -J "NNAgent8Explorer-K-10" -P "NNAgent8Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
+bsub -o "outputs/Explorer-K-10/NNAgent9Explorer-K-10.md" -J "NNAgent9Explorer-K-10" -P "NNAgent9Explorer-K-10 2000 10 NNAgent 1 1 1 1 0 Explorer-K-10" < submit.sh
--- generateExperiments.py
+++ generateExperiments.py
@@ -9,5 +9,6 @@ def genExperiments(name, agent, gameLength, adAgent, explore, doTrain, impala, c
         file.write(f'bsub -o "outputs/{name}/{agent}{str(i)}{name}.md" -J "{agent}{str(i)}{name}" -P "{agent}{str(i)}{name} {gameLength} {adAgent} {agent} {int(explore)} {int(doTrain)} {int(impala)} {int(calcprobs)} {int(minmax)} {name}" < submit.sh\n')
 
 
-genExperiments('Explorer-off', 'NNAgent', 2000, 10, False, True, True, True, False, 10)
-genExperiments('Explorer-on-no-temp', 'NNAgent', 2000, 10, True, True, True, True, False, 10)
+genExperiments('Explorer-K-10', 'NNAgent', 2000, 10, True, True, True, True, False, 10)
+# genExperiments('Explorer-K-50', 'NNAgent', 2000, 10, True, True, True, True, False, 10)
+# genExperiments('Explorer-K-200', 'NNAgent', 2000, 10, True, True, True, True, False, 10)
