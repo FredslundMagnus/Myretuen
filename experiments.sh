02131decf3c55b39a289e04a7a3922100a0fb7a2
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-17 16:10:08 +0100

    exp

--- experiments.sh
+++ experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-mkdir outputs/Explorer-K-500
-bsub -o "outputs/Explorer-K-500/NNAgent0Explorer-K-500.md" -J "NNAgent0Explorer-K-500" -P "NNAgent0Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent1Explorer-K-500.md" -J "NNAgent1Explorer-K-500" -P "NNAgent1Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent2Explorer-K-500.md" -J "NNAgent2Explorer-K-500" -P "NNAgent2Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent3Explorer-K-500.md" -J "NNAgent3Explorer-K-500" -P "NNAgent3Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent4Explorer-K-500.md" -J "NNAgent4Explorer-K-500" -P "NNAgent4Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent5Explorer-K-500.md" -J "NNAgent5Explorer-K-500" -P "NNAgent5Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent6Explorer-K-500.md" -J "NNAgent6Explorer-K-500" -P "NNAgent6Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent7Explorer-K-500.md" -J "NNAgent7Explorer-K-500" -P "NNAgent7Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent8Explorer-K-500.md" -J "NNAgent8Explorer-K-500" -P "NNAgent8Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
-bsub -o "outputs/Explorer-K-500/NNAgent9Explorer-K-500.md" -J "NNAgent9Explorer-K-500" -P "NNAgent9Explorer-K-500 4000 10 NNAgent 1 1 1 1 0 Explorer-K-500" < submit.sh
+mkdir outputs/Explorer-K-250-new-loss
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent0Explorer-K-250-new-loss.md" -J "NNAgent0Explorer-K-250-new-loss" -P "NNAgent0Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent1Explorer-K-250-new-loss.md" -J "NNAgent1Explorer-K-250-new-loss" -P "NNAgent1Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent2Explorer-K-250-new-loss.md" -J "NNAgent2Explorer-K-250-new-loss" -P "NNAgent2Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent3Explorer-K-250-new-loss.md" -J "NNAgent3Explorer-K-250-new-loss" -P "NNAgent3Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent4Explorer-K-250-new-loss.md" -J "NNAgent4Explorer-K-250-new-loss" -P "NNAgent4Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent5Explorer-K-250-new-loss.md" -J "NNAgent5Explorer-K-250-new-loss" -P "NNAgent5Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent6Explorer-K-250-new-loss.md" -J "NNAgent6Explorer-K-250-new-loss" -P "NNAgent6Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent7Explorer-K-250-new-loss.md" -J "NNAgent7Explorer-K-250-new-loss" -P "NNAgent7Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent8Explorer-K-250-new-loss.md" -J "NNAgent8Explorer-K-250-new-loss" -P "NNAgent8Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
+bsub -o "outputs/Explorer-K-250-new-loss/NNAgent9Explorer-K-250-new-loss.md" -J "NNAgent9Explorer-K-250-new-loss" -P "NNAgent9Explorer-K-250-new-loss 2000 10 NNAgent 1 1 1 1 0 Explorer-K-250-new-loss" < submit.sh
