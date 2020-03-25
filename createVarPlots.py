8b4e2732df8e378ada8e288ec1749846225ca575
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-25 14:10:02 +0100

    plot creater

---
+++ createVarPlots.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+
+subdirs = set()
+for s, _, _ in os.walk(os.curdir):
+    if s[:10] == '.\outputs\\':
+        for s1, _, _ in os.walk(s):
+            if s1[-3:] == 'csv':
+                subdirs.add(s1)
+
+for directory in subdirs:
+    for _, _, files in os.walk(directory):
+        for i, filename in enumerate(files):
+
+            print(filename)
+            data = pd.read_csv(directory + '\\' + filename, header=None)
+            print(data)
