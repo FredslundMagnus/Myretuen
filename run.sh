0d0cecfff9b2207d5762df32241f1a74a1837bce
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-25 15:06:58 +0100

    test

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git pull
+python generateExperiments.py
 chmod +x experiments.sh
 ./experiments.sh
--- save.sh
+++ save.sh
@@ -2,4 +2,8 @@
 git add .
 git commit -m "Just Trained"
 git pull
+# python createVarPlots.py
+# git add .
+# git commit -m "Just Created Plots"
+# git pull
 git push
