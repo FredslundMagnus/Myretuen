0219cb94741b08fa77927e888caa9dd5095da181
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-03-25 15:03:05 +0100

    created mean plots

--- createVarPlots.py
+++ createVarPlots.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import re
 
 subdirs = set()
 for s, _, _ in os.walk(os.curdir):
@@ -9,25 +10,62 @@ for s, _, _ in os.walk(os.curdir):
         for s1, _, _ in os.walk(s):
             if s1[-3:] == 'csv':
                 subdirs.add(s1)
-
 Elo = None
+EloOverTime = None
+agent = 'Error'
 for directory in subdirs:
     for _, _, files in os.walk(directory):
         for filename in files:
+            match = re.match(r"([a-z]+)([0-9]+)", filename, re.I)
+            if match:
+                items = match.groups()
+                agent = items[0]
+
             curentname = filename.split('-')[-1][:-4]
             data = pd.read_csv(directory + '\\' + filename, header=None)
             data = np.array(data).reshape(data.shape[0], data.shape[1], 1)
-            if curentname == 'Elo2':
+            if curentname == 'Elo':
                 if Elo is None:
                     Elo = data
                 else:
                     Elo = np.concatenate((Elo, data), axis=-1)
-# Elo = np.mean(Elo, axis=2)
-# plt.plot(green, label=controller.agents['green'].name)
-# plt.plot(red, label='RandomAgent')
-# if debuggerMode:
-#     a = np.array([green, red])
-#     np.savetxt(f"outputs/{Thename}/csv/{nameOfRun}-Elo.csv", a, delimiter=',', fmt='%d')
-# plt.ylim((700, 2000))
-# plot('Elo_Rating', labels=True)
-# print(Elo)
+            elif curentname == 'EloOverTime':
+                if EloOverTime is None:
+                    EloOverTime = data
+                else:
+                    EloOverTime = np.concatenate((EloOverTime, data), axis=-1)
+    name = directory.split('\\')[-2]
+
+    if Elo is not None:
+        Mean = np.mean(Elo, axis=2)
+        Std = np.std(Elo, axis=2)
+        y = Mean[0, :]
+        sd = Std[0, :]
+        x = np.arange(1, len(y) + 1)
+        plt.plot(x, y, lw=2, label=agent, color='b')
+        plt.plot(x, Mean[1, :], lw=2, label='RandomAgent', color='r')
+        plt.fill_between(x, y + sd, y - sd, facecolor='b', alpha=0.5)
+        plt.legend()
+        plt.ylim((700, 2000))
+        plt.title(f'{name} - Elo Rating')
+        plt.xlabel('Players')
+        plt.ylabel('Elo')
+        plt.savefig(f'outputs/{name}/' + f'{name} - Elo Rating' + '.png')
+        plt.clf()
+
+    if EloOverTime is not None:
+        Mean = np.mean(EloOverTime, axis=2)
+        Std = np.std(EloOverTime, axis=2)
+        y = Mean[0, :]
+        sd = Std[0, :]
+        x = np.arange(1, len(y) + 1)
+        plt.plot(x, y, lw=2, label=agent, color='b')
+        plt.plot(x, Mean[1, :], lw=2, label='RandomAgent', color='r')
+        plt.fill_between(x, y + sd, y - sd, facecolor='b', alpha=0.5)
+        plt.legend()
+        plt.ylim((700, 2000))
+        plt.title(f'{name} - Elo Rating over Time')
+        plt.xlabel('Games')
+        plt.ylabel('Elo')
+        plt.savefig(f'outputs/{name}/' + f'{name} - Elo Rating over Time' + '.png')
+        plt.clf()
