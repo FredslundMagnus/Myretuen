244420d14bf785eb7c7eb263ff0ebaee935151a7
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-04-05 02:14:39 +0200

    fix

--- Agents/cleverRandom.py
+++ Agents/cleverRandom.py
@@ -1,3 +1,7 @@
+import random
+from Agents.agent import Agent
+
+
 class CleverRandom(Agent):
     def __init__(self, explore=False, doTrain=False, impala=False, calcprobs=False, minmax=False, lossf=None, K=None, dropout=None, alpha=None, discount=None, lambd=None, lr=None, TopNvalues=6, cutOffdepth=1, ValueCutOff=5, ValueDiffCutOff=2, ProbabilityCutOff=0.03, historyLength=20, startAfterNgames=20, batchSize=20, sampleLenth=5):
         self.setup(False, False, False, calcprobs, minmax, None, None, None, None, None, None, None, 'CleverRandom', TopNvalues, cutOffdepth, ValueCutOff, ValueDiffCutOff, ProbabilityCutOff, historyLength, startAfterNgames, batchSize, sampleLenth)
