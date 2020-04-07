28ec101a2d5baa0e724071252cbb8fcf73b68eb4
TheDingodile <TheDingodile@gmail.com>
2020-04-07 13:28:53 +0200

    reward-final

--- move.py
+++ move.py
@@ -175,7 +175,7 @@ class Move():
 
             enemies = [ant for ant in ants if ant.color == self.end.ants[-1].color]
             if all([not x.isAlive for x in enemies]):
-                sim_win_reward += 3 * len(ants) / 2 + 10
+                sim_win_reward += 2 * len(ants) + 10
         else:
             probofstate = 1 - probofstate
             if probofstate != 1:
@@ -194,7 +194,7 @@ class Move():
 
             allies = [ant for ant in ants if ant.color == theAnt.color]
             if all([not x.isAlive for x in allies]):
-                sim_win_reward -= 3 * len(ants) / 2 + 10
+                sim_win_reward -= 2 * len(ants) + 10
 
         self.game.prob.CalculateWinChance()
         self.giveantsprobabilities(self.game.prob.probmatrix, ants)
