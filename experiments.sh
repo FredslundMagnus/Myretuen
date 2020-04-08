0f1e486f62af57d7ae3b6a10c900dc743f1c031a
TheDingodile <TheDingodile@gmail.com>
2020-04-08 16:47:36 +0200

    test4

--- experiments.sh
+++ experiments.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-mkdir outputs/HistoryLength1
-mkdir outputs/HistoryLength1/csv
-mkdir outputs/HistoryLength1/trained
-mkdir outputs/HistoryLength1/TrainingCurve
-mkdir outputs/HistoryLength1/Weights
-mkdir outputs/HistoryLength1/Elo_Rating
-mkdir outputs/HistoryLength1/Increase_in_Elo_over_time
-bsub -o "outputs/HistoryLength1/NNAgent0HistoryLength1.md" -J "NNAgent0HistoryLength1" -P "NNAgent0HistoryLength1 15 10 NNAgent 1 0 1 1 0 HistoryLength1" < submit.sh
-bsub -o "outputs/HistoryLength1/NNAgent1HistoryLength1.md" -J "NNAgent1HistoryLength1" -P "NNAgent1HistoryLength1 15 10 NNAgent 1 0 1 1 0 HistoryLength1" < submit.sh
-bsub -o "outputs/HistoryLength1/NNAgent2HistoryLength1.md" -J "NNAgent2HistoryLength1" -P "NNAgent2HistoryLength1 15 10 NNAgent 1 0 1 1 0 HistoryLength1" < submit.sh
-bsub -o "outputs/HistoryLength1/NNAgent3HistoryLength1.md" -J "NNAgent3HistoryLength1" -P "NNAgent3HistoryLength1 15 10 NNAgent 1 0 1 1 0 HistoryLength1" < submit.sh
-bsub -o "outputs/HistoryLength1/NNAgent4HistoryLength1.md" -J "NNAgent4HistoryLength1" -P "NNAgent4HistoryLength1 15 10 NNAgent 1 0 1 1 0 HistoryLength1" < submit.sh
+mkdir outputs/NOTHistoryLength1
+mkdir outputs/NOTHistoryLength1/csv
+mkdir outputs/NOTHistoryLength1/trained
+mkdir outputs/NOTHistoryLength1/TrainingCurve
+mkdir outputs/NOTHistoryLength1/Weights
+mkdir outputs/NOTHistoryLength1/Elo_Rating
+mkdir outputs/NOTHistoryLength1/Increase_in_Elo_over_time
+bsub -o "outputs/NOTHistoryLength1/NNAgent0NOTHistoryLength1.md" -J "NNAgent0NOTHistoryLength1" -P "NNAgent0NOTHistoryLength1 5 10 NNAgent 1 0 1 1 0 NOTHistoryLength1 -lossf MME -K 1000 -discount 0.98 -lambda 0.8 -lr 0.0001" < submit.sh
+bsub -o "outputs/NOTHistoryLength1/NNAgent1NOTHistoryLength1.md" -J "NNAgent1NOTHistoryLength1" -P "NNAgent1NOTHistoryLength1 5 10 NNAgent 1 0 1 1 0 NOTHistoryLength1 -lossf MME -K 1000 -discount 0.98 -lambda 0.8 -lr 0.0001" < submit.sh
+bsub -o "outputs/NOTHistoryLength1/NNAgent2NOTHistoryLength1.md" -J "NNAgent2NOTHistoryLength1" -P "NNAgent2NOTHistoryLength1 5 10 NNAgent 1 0 1 1 0 NOTHistoryLength1 -lossf MME -K 1000 -discount 0.98 -lambda 0.8 -lr 0.0001" < submit.sh
+bsub -o "outputs/NOTHistoryLength1/NNAgent3NOTHistoryLength1.md" -J "NNAgent3NOTHistoryLength1" -P "NNAgent3NOTHistoryLength1 5 10 NNAgent 1 0 1 1 0 NOTHistoryLength1 -lossf MME -K 1000 -discount 0.98 -lambda 0.8 -lr 0.0001" < submit.sh
+bsub -o "outputs/NOTHistoryLength1/NNAgent4NOTHistoryLength1.md" -J "NNAgent4NOTHistoryLength1" -P "NNAgent4NOTHistoryLength1 5 10 NNAgent 1 0 1 1 0 NOTHistoryLength1 -lossf MME -K 1000 -discount 0.98 -lambda 0.8 -lr 0.0001" < submit.sh
