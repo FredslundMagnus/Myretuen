b29177986bcc6150339e8a38cd76a0166c85ba93
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-04-19 09:22:51 +0200

    fix

--- Agents/randomAgent.py
+++ Agents/randomAgent.py
@@ -4,8 +4,8 @@ from Agents.agent import Agent
 
 
 class RandomAgent(Agent):
-    def __init__(self, explore=False, doTrain=False, impala=False, calcprobs=False, minmax=False, lossf=None, K=None, dropout=None, alpha=None, discount=None, lambd=None, lr=None, TopNvalues=6, cutOffdepth=1, ValueCutOff=5, ValueDiffCutOff=2, ProbabilityCutOff=0.03, historyLength=20, startAfterNgames=20, batchSize=20, sampleLenth=5, network=None, analyse=False):
-        self.setup(False, False, False, calcprobs, minmax, None, None, None, None, None, None, None, 'RandomAgent', TopNvalues, cutOffdepth, ValueCutOff, ValueDiffCutOff, ProbabilityCutOff, historyLength, startAfterNgames, batchSize, sampleLenth, None, analyse)
+    def __init__(self, explore=False, doTrain=False, impala=False, calcprobs=False, minmax=False, lossf=None, K=None, dropout=None, alpha=None, discount=None, lambd=None, lr=None, TopNvalues=6, cutOffdepth=1, ValueCutOff=5, ValueDiffCutOff=2, ProbabilityCutOff=0.03, historyLength=20, startAfterNgames=20, batchSize=20, sampleLenth=5, network=None, analyse=False, montocarlo=False):
+        self.setup(False, False, False, calcprobs, minmax, None, None, None, None, None, None, None, 'RandomAgent', TopNvalues, cutOffdepth, ValueCutOff, ValueDiffCutOff, ProbabilityCutOff, historyLength, startAfterNgames, batchSize, sampleLenth, None, analyse, montocarlo)
         self.rating = 1000
 
     def choose(self, actions):
