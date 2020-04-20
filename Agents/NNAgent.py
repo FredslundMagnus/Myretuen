a378aeb6ccfbb4824ef40798f2ed36331f9b7c3e
TheDingodile <TheDingodile@gmail.com>
2020-04-20 12:05:21 +0200

    endreward_fixed

--- Agents/NNAgent.py
+++ Agents/NNAgent.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 
 
 class NNAgent(Agent):
-    def __init__(self, explore=False, doTrain=False, impala=False, calcprobs=True, minmax=False, lossf='MME', K=1000, dropout=0.0, alpha=None, discount=0.995, lambd=0.9, lr=0.0005, TopNvalues=2, cutOffdepth=5, ValueCutOff=50, ValueDiffCutOff=15, ProbabilityCutOff=0.001, historyLength=25, startAfterNgames=25, batchSize=100, sampleLenth=5, network=[50, 25, 10], analyse=False, montecarlo=True):
+    def __init__(self, explore=False, doTrain=False, impala=False, calcprobs=True, minmax=False, lossf='MME', K=2000, dropout=0.0, alpha=None, discount=0.995, lambd=0.9, lr=0.0001, TopNvalues=2, cutOffdepth=5, ValueCutOff=50, ValueDiffCutOff=15, ProbabilityCutOff=0.001, historyLength=2, startAfterNgames=2, batchSize=500, sampleLenth=5, network=[50, 25, 10], analyse=False, montecarlo=True):
         self.setup(explore, doTrain, impala, calcprobs, minmax, lossf, K, dropout, None, discount, lambd, lr, 'NNAgent', TopNvalues, cutOffdepth, ValueCutOff, ValueDiffCutOff, ProbabilityCutOff, historyLength, startAfterNgames, batchSize, sampleLenth, network, analyse, montecarlo)
         self.optimizer = None
         self.optimise = True
@@ -32,7 +32,7 @@ class NNAgent(Agent):
     def train(self, reward, previousState, newState):
         Vst = self.value(previousState, return_float=False)
         Vstnext = self.value(newState, return_float=True)
-        label = torch.FloatTensor([reward + self.discount * Vstnext])
+        label = torch.FloatTensor([reward + self.discount * Vstnext]) if abs(reward) != 81 else torch.FloatTensor([reward])
         if self.lossf == 'Abs':
             criterion = nn.L1Loss()
         else:
