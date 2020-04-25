488ddb000eed77d3aea0984473e75e056ae2622b
TheDingodile <TheDingodile@gmail.com>
2020-04-25 12:31:40 +0200

    new_plots_lambda_discount

---
+++ plotgenerators/Lamd0-01.py
@@ -0,0 +1,9 @@
+from plots import EloPlot, Data
+import numpy as np
+
+l = [('LAMBDA-0.01_DISCOUNT-0.01', 'Discount=0.01, lambda=0.01', '#9C27B0'), ('LAMBDA-0.01_DISCOUNT-0.1', 'Discount=0.1, lambda=0.01', '#2196F3'), ('LAMBDA-0.01_DISCOUNT-0.5', 'Discount=0.5, lambda=0.01', '#4CAF50'), ('LAMBDA-0.01_DISCOUNT-0.9', 'Discount=0.9, lambda=0.01', '#F44336'), ('LAMBDA-0.01_DISCOUNT-0.99', 'Discount=0.99, lambda=0.01', '#228B22')]
+
+with EloPlot(title=f'Lambda and Discount - lambda 0.01', saves=[f'GoodPlots/LambdaDiscountLAMBD001.png']) as plt:
+    for EloOverTime, name, agent, color in Data([f".\\outputs\\{a[0]}\\csv" for a in l], colors=[a[2] for a in l], elo=False):
+        name = [a[1] for a in l if a[0] == name][0]
+        plt.varPlot(plt, EloOverTime, name, color)
