49c58a084db0e28b3098a2d875b996cb7b6cc12b
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-04-28 22:56:48 +0200

    plots

--- plotgenerators/agentsplot.py
+++ plotgenerators/agentsplot.py
@@ -1,9 +1,11 @@
 from plots import EloPlot, Data
 import numpy as np
 
-l = [('new-agent-SL', 'Simple Linear', '#9C27B0'), ('new-agent-LA', 'Linear Approximation', '#2196F3'), ('IMP-sample-length8-hist30', 'Neural Network', '#4CAF50')]
+for discount in [0.5, 0.75, 0.95]:
 
-with EloPlot(title=f'Agents - Elo Rating', saves=[f'GoodPlots/Agents.png']) as plt:
-    for EloOverTime, name, agent, color in Data([f".\\outputs\\{a[0]}\\csv" for a in l], colors=[a[2] for a in l], elo=False):
-        name = [a[1] for a in l if a[0] == name][0]
-        plt.varPlot(plt, EloOverTime, name, color)
+    l = [(f'SL-discount-{discount}-NoTrain-alpha-0.00001', 'Simple Linear', '#9C27B0'), (f'LA-discount-{discount}-NoTrain-alpha-0.000001', 'Linear Approximation', '#2196F3'), (f'NN-discount-{discount}-NoTrain-lr-0.0002', 'Neural Network', '#4CAF50')]
+
+    with EloPlot(title=f'Agents, discount={discount} - Elo Rating', saves=[f'GoodPlots/Agents-{discount}.png']) as plt:
+        for EloOverTime, name, agent, color in Data([f".\\outputs\\{a[0]}\\csv" for a in l], colors=[a[2] for a in l], elo=False):
+            name = [a[1] for a in l if a[0] == name][0]
+            plt.varPlot(plt, EloOverTime, name, color)
