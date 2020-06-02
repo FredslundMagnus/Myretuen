6c83e41402d221ac0776ca88d04577cf0f7009bb
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-06-02 13:30:48 +0200

    plots

--- plotgenerators/impalaplots.py
+++ plotgenerators/impalaplots.py
@@ -11,7 +11,7 @@ for chooser in ['random', 'weighted']:
 
 for chooser in ['random', 'weighted']:
     selfplay = 50
-    l = [(f'NN-Selfplay-{selfplay}-{chooser}-lr=0.0002-K=2000', 'No Impala', '#673ab7'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-50-20-calcprobs', 'NN 50 20', '#9C27B0'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-100-10-calcprobs', 'NN 100 10', '#2196F3'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-200-5-calcprobs', 'NN 200 5', '#4CAF50'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-500-2-calcprobs', 'NN 500 20', '#795548'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-1000-1-calcprobs', 'NN 1000 1', '#ffeb3b')]
+    l = [(f'NN-Selfplay-{selfplay}-{chooser}-lr=0.0002-K=2000-calcprobs', 'No Impala', '#673ab7'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-50-20-calcprobs', 'NN 50 20', '#9C27B0'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-100-10-calcprobs', 'NN 100 10', '#2196F3'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-200-5-calcprobs', 'NN 200 5', '#4CAF50'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-500-2-calcprobs', 'NN 500 20', '#795548'), (f'NN-Selfplay-{selfplay}-{chooser}-impala-20-20-1000-1-calcprobs', 'NN 1000 1', '#ffeb3b')]
 
     with EloPlot(title=f'Chooserfunction, selfplay={selfplay} - Calcprobs - Elo Rating', saves=[f'GoodPlots/Impala-{chooser}-1000-calcprobs.png']) as plt:
         for EloOverTime, name, agent, color in Data([f".\\outputs\\{a[0]}\\csv" for a in l], colors=[a[2] for a in l], elo=False):
