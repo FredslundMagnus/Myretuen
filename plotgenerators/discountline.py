cd039d362c0fcddf541b0144afe52b73a30cefb5
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-06-03 09:20:55 +0200

    plots

---
+++ plotgenerators/discountline.py
@@ -0,0 +1,42 @@
+from plots import Data, allDirs, find_number, colors
+from matplotlib import pyplot as plt
+import numpy as np
+
+xs = [3000, 4000]  # [1000, 2000, 3000, 4000]
+d = 'Discount-'
+discounts = np.arange(start=0.7, stop=0.99, step=0.01)
+means = np.arange(start=0.7, stop=0.99, step=0.01)
+var = np.arange(start=0.7, stop=0.99, step=0.01)
+1 / 0
+files, ms, ss = [], {x: np.arange(start=0.7, stop=0.99, step=0.01) for x in xs}, {x: np.arange(start=0.7, stop=0.99, step=0.01) for x in xs}
+
+for filename in allDirs():
+    if d in filename:
+        files.append(filename)
+
+
+def meanAround(data, x, width=10):
+    return data[0, x - width:x + width, :].mean()
+
+
+def stdAround(data, x, width=10):
+    return data[0, x - width:x + width, :].std()
+
+
+for EloOverTime, name, agent, color in Data(files, elo=False):
+    dis = find_number(name, d)
+    for x in xs:
+        ms[x][discounts == dis] = meanAround(EloOverTime, x)
+        ss[x][discounts == dis] = stdAround(EloOverTime, x)
+
+
+for x, mean in ms.items():
+    std = ss[x]
+    plt.plot(discounts, mean, lw=2, label="Discount", color=colors[0])
+    plt.fill_between(discounts, mean + std, mean - std, facecolor=colors[0], alpha=0.5)
+    plt.title(f'Discount-{x}')
+    plt.xlabel('Discount')
+    plt.ylabel('Elo')
+    plt.legend(loc='upper left')
+    plt.savefig(f'GoodPlots/Discount-line-{x}.png')
+    plt.clf()
