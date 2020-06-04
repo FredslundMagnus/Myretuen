84b9de4b5bfed640d3f84c75521c3b8aae5bb935
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-06-04 16:46:36 +0200

    plots

--- Analyse/explorerAna.py
+++ Analyse/explorerAna.py
@@ -3,7 +3,7 @@ import numpy as np
 # gameLength   gameElo   minBase   dinBase   meandistMine   meandistDine   isInFront   carryEnimy   carryAlly
 
 agents = [('NN-Selfplay-20-random-lr=0.0002-K=2000', 'Explore, random, 20'), ('NN-Selfplay-20-random-lr=0.0002', 'No explore, random, 20')]
-for feature in ['minBase', 'dinBase', 'meandistMine', 'meandistDine', 'isInFront', 'carryEnimy', 'carryAlly']:
+for feature in ['gameLength', 'minBase', 'dinBase', 'meandistMine', 'meandistDine', 'isInFront', 'carryEnimy', 'carryAlly']:
     with AnalysePlot(feature=feature, title='Placeholder', savepos=f'Explorer-{feature}') as plt:
         for i, names in enumerate(agents):
             mean, sd = DataFinder(names[0])
