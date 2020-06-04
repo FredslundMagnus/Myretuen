31fed44f33fdb729fbc4d4ea4db5c5862bf54b55
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-06-04 16:42:30 +0200

    fix

--- Analyse/explorerAna.py
+++ Analyse/explorerAna.py
@@ -2,7 +2,7 @@ from analyses import DataFinder, colors, AnalysePlot
 import numpy as np
 # gameLength   gameElo   minBase   dinBase   meandistMine   meandistDine   isInFront   carryEnimy   carryAlly
 
-agents = [('NN-Selfplay-20-random-lr=0.0002-K=2000', 'Explore, random, 20'), ('NN-Selfplay-20-random-lr=0.0002', 'No explore, incremental, 20')]
+agents = [('NN-Selfplay-20-random-lr=0.0002-K=2000', 'Explore, random, 20'), ('NN-Selfplay-20-random-lr=0.0002', 'No explore, random, 20')]
 for feature in ['minBase', 'dinBase', 'meandistMine', 'meandistDine', 'isInFront', 'carryEnimy', 'carryAlly']:
     with AnalysePlot(feature=feature, title='Placeholder', savepos=f'Explorer-{feature}') as plt:
         for i, names in enumerate(agents):
