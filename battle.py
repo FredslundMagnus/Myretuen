1298a6c5f7a8788eac300dd91b43513985a36151
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-06-11 19:35:32 +0200

    Clean

--- battle.py
+++ battle.py
@@ -1,7 +1,8 @@
 from agents import *
 from game import Myretuen, Controller
 
-fruit = False
-env = Myretuen(color1='red', color2='green', nAnts=10, fruits=fruit)
-controller = Controller(env=env, agent1=Opponent(PlayerAgent()), agent2=NNAgent(minmax=True, explore=False, montecarlo=False, TopNvalues=3, cutOffdepth=3, ValueCutOff=25, ValueDiffCutOff=10).loadModel('2200ELOFRUIT' if fruit else '2512ELO'))
-controller.run()
+# Her kan man ændre indstillingerne
+env = Myretuen(color1='red', color2='green', fruits=True)
+
+# Her starter spillet
+Controller(env=env, agent1=Opponent(PlayerAgent()), agent2=NNAgent(minmax=True, TopNvalues=3, cutOffdepth=3, ValueCutOff=25, ValueDiffCutOff=10).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')).run()
