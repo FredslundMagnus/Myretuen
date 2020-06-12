a6da9a02a761dc7a463524133e8af12c2bbcc506
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-06-12 15:45:25 +0200

    Battle

--- battle.py
+++ battle.py
@@ -1,8 +1,13 @@
 from agents import *
 from game import Myretuen, Controller
 
-# Her kan man ændre indstillingerne
-env = Myretuen(color1='red', color2='green', fruits=True)
+n = 5
+fruits = True
+playerStarts = True
 
-# Her starter spillet, med en player mod den bedste AI inden for hver af modesne
-Controller(env=env, agent1=Opponent(PlayerAgent()), agent2=NNAgent(minmax=True).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')).run()
+if playerStarts:
+    env = Myretuen(color1='blue', color2='yellow', fruits=fruits)
+    Controller(env=env, agent1=Opponent(PlayerAgent()), agent2=NNAgent(minmax=True).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')).run(NGames=n)
+else:
+    env = Myretuen(color1='red', color2='green', fruits=fruits)
+    Controller(env=env, agent1=Opponent(NNAgent(minmax=True).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')), agent2=PlayerAgent()).run(NGames=n)
