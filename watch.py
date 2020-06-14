d9e6db17c35ab2cc3102efab94d31e33f937d449
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-06-14 13:02:38 +0200

    Watch

--- watch.py
+++ watch.py
@@ -1,9 +1,8 @@
 from agents import *
 from game import Myretuen, Controller
 
-n = 5
 fruits = True
-minmax = True
+minmax = False
 
-env = Myretuen(color1='blue', color2='yellow', fruits=fruits)
-Controller(env=env, agent1=Opponent(NNAgent(minmax=minmax).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')), agent2=NNAgent(minmax=minmax).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')).run(NGames=n)
+env = Myretuen(color1='green', color2='blue', fruits=fruits)
+Controller(env=env, agent1=Opponent(NNAgent(minmax=minmax).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')), agent2=NNAgent(minmax=minmax).loadModel('2200ELOFRUIT' if env.splitvariant else '2512ELO')).run(timeDelay=2)
